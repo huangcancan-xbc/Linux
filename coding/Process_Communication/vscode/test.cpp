0fe1af58299f88f9386ce71649427691d1302fd9
huangcancan-xbc <hc1960074081@qq.com>
2025-07-29 12:11:02 +0800

    VS code

---
+++ coding/Process_Communication/vscode/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<< "Hello Linux！" <<endl;
+    cout<< "Hello Linux！" <<endl;
+    cout<< "Hello Linux！" <<endl;
+    cout<< "Hello Linux！" <<endl;
+    cout<< "Hello Linux！" <<endl;
+    
+    return 0;
+}
